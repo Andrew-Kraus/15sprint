4069b5771fdda232163a6a7e0d0c6899391ec4ad
Andrew Kraus <KalkalovichIvan@yandex.ru>
2020-10-22 02:24:24 +0300

    v0.0.4

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 const getUser = require('./routes/users');
 const getCard = require('./routes/cards');
@@ -22,6 +23,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+      throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/users', getUser);
 app.use('/cards', getCard);
 app.post('/signin', login);
@@ -32,6 +39,10 @@ app.use('/', (req, res) => {
   res.status(404).end(JSON.stringify({ message: 'Запрашиваемый ресурс не найден' }), 'utf8');
 });
 
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
 
 app.use(errorLogger);
 app.use(errors());
