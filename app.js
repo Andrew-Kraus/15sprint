a6e9b132e6df35c8e46354ebc7d2eb3de0b0c915
Andrew Kraus <KalkalovichIvan@yandex.ru>
2020-10-22 00:50:39 +0300

    v0.0.2

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
 const app = express();
 const getUser = require('./routes/users');
 const getCard = require('./routes/cards');
